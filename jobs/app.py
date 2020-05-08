f6de391f33984f1fd54a3c107d774d6f5c757bc1
Andrew Schoenfeld <andrew@schoenfeld.jp>
2020-05-08 12:43:32 -1000

    Completed Module 5
    Display Individual Jobs: 6 tasks

--- jobs/app.py
+++ jobs/app.py
@@ -28,6 +28,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
@@ -40,6 +41,14 @@ def close_connection(exception):
 @app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, '
-                'employer.id as employer_id, '
-                'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+                       'employer.id as employer_id, '
+                       'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, '
+                      'employer.id as employer_id, '
+                      'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',
+                      [job_id], single=True)
+    return render_template('job.html', job=job)
